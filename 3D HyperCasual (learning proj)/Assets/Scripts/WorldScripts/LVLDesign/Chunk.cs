b453265badcf1292a14075597aa9e8e21cdeb38e
TaschenJude <donyadonya1488@gmail.com>
2025-11-09 15:25:51 +0200

    Remake of Chunk and ChunkSpawner logic
    -Updated logic of spawns

--- 3D HyperCasual (learning proj)/Assets/Scripts/WorldScripts/LVLDesign/Chunk.cs
+++ 3D HyperCasual (learning proj)/Assets/Scripts/WorldScripts/LVLDesign/Chunk.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -51,28 +51,162 @@ public class Chunk : MonoBehaviour
             Vector3 randomOffset = new Vector3(Random.Range(-1.8f, 1.8f), 0.5f, Random.Range(-1.8f, 1.8f));
             Instantiate(_coinPrefab, point.position + randomOffset, point.rotation);
         }
-    }
-    //ЗАЛИШИТИ (ПОКИ ЯК КОМЕНТАР, ДЛЯ ПОДАЛЬШОЇ ЛОГІКИ)
-    /*private IEnumerator SpawnEnemies(Transform point)
-    {
-        int count = Random.Range(_enemiesMinCount, _enemiesMaxCount);
-        for (int i = 0; i < count; i++)
-        {
-            Instantiate(_enemyPrefab, point.position, point.rotation, transform);
-            yield return new WaitForSeconds(_enemySpawnTimer);
-        }
     }*/
-    //ЗАЛИШИТИ
-    IEnumerator EnemySpawnCoroutine(GameObject randomPrefab, Transform randomPoint) 
-    { 
-        int randomCount = Random.Range(_enemiesMinCount, _enemiesMaxCount); 
-        for (int i = 0; i < randomCount; i++) 
-        { Instantiate(randomPrefab, randomPoint.position, randomPoint.rotation, transform); 
-            yield return new WaitForSeconds(_enemySpawnTimer); 
-        } 
+//ЗАЛИШИТИ (ПОКИ ЯК КОМЕНТАР, ДЛЯ ПОДАЛЬШОЇ ЛОГІКИ)
+/*private IEnumerator SpawnEnemies(Transform point)
+{
+    int count = Random.Range(_enemiesMinCount, _enemiesMaxCount);
+    for (int i = 0; i < count; i++)
+    {
+        Instantiate(_enemyPrefab, point.position, point.rotation, transform);
+        yield return new WaitForSeconds(_enemySpawnTimer);
+    }
+}*/
+//ЗАЛИШИТИ
+/*IEnumerator EnemySpawnCoroutine(GameObject randomPrefab, Transform randomPoint) 
+{ 
+    int randomCount = Random.Range(_enemiesMinCount, _enemiesMaxCount); 
+    for (int i = 0; i < randomCount; i++) 
+    { Instantiate(randomPrefab, randomPoint.position, randomPoint.rotation, transform); 
+        yield return new WaitForSeconds(_enemySpawnTimer); 
+    } 
+}
+
+//ЗАЛИШИТИ
+public void OnCollisionEnter(Collision collision)
+{
+    if (collision.gameObject.CompareTag("Player"))
+    {
+        chunkSpawner.StepOnChunk(this);
+    }
+}
+}*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum SpawnType { Enemy, BuffOrDebuff }
+
+public class Chunk : MonoBehaviour
+{
+    public ChunkSpawner chunkSpawner;
+    [SerializeField] private Transform[] _pointsToSpawn;
+    [SerializeField] private Transform[] _coinSpawnPoints;
+    [SerializeField] private GameObject[] _prefabsToSpawn;
+    [SerializeField] private GameObject _coinPrefab;
+
+    [Header("Enemy Settings")]
+    [SerializeField] private int _enemiesBaseMinCount = 3;
+    [SerializeField] private int _enemiesBaseMaxCount = 9;
+    [SerializeField] private float _enemySpawnTimer;
+
+    private void Start()
+    {
+        if (_pointsToSpawn == null || _pointsToSpawn.Length == 0)
+        {
+            InitializeSpawnPoints();
+        }
+        SpawnObjectsInChunk();
+        SpawnCoins(); 
+    }
+    private void InitializeSpawnPoints()
+    {
+        _pointsToSpawn = GetComponentsInChildren<Transform>();
+    }
+    private void SpawnObjectsInChunk()
+    {
+        Transform randomPoint = _pointsToSpawn[Random.Range(0, _pointsToSpawn.Length)];
+        SpawnType type = (Random.Range(0, 2) == 0) ? SpawnType.Enemy : SpawnType.BuffOrDebuff;
+        switch (type)
+        {
+            case SpawnType.Enemy:
+                
+                SpawnEnemyLogic(randomPoint);
+                break;
+
+            case SpawnType.BuffOrDebuff:
+                
+                GameObject randomPrefab = _prefabsToSpawn[Random.Range(0, _prefabsToSpawn.Length)];
+                SpawnBuffOrDebuffLogic(randomPrefab, randomPoint);
+                break;
+        }
+    }
+    private SpawnType DetermineSpawnType(GameObject prefab)
+    {
+        if (prefab.CompareTag("Enemy") || prefab.CompareTag("Shooting Enemy"))
+        {
+            return SpawnType.Enemy;
+        }
+        return SpawnType.BuffOrDebuff;
+    }
+    private void SpawnEnemyLogic(Transform spawnPoint)
+    {
+        GameObject[] availableEnemies;
+        if (chunkSpawner.currentWave >= chunkSpawner.GetShootingStartWave())
+        {
+            List<GameObject> combinedList = new List<GameObject>();
+            combinedList.AddRange(chunkSpawner.GetMeleePrefabs());
+            combinedList.AddRange(chunkSpawner.GetShootingPrefabs());
+            availableEnemies = combinedList.ToArray();
+        }
+        else
+        {
+            availableEnemies = chunkSpawner.GetMeleePrefabs();
+        }
+
+        GameObject enemyToSpawn = availableEnemies[Random.Range(0, availableEnemies.Length)];
+        int progression = chunkSpawner.currentWave / 10;
+        int enemiesMaxCount = _enemiesBaseMaxCount + progression;
+        StartCoroutine(EnemySpawnCoroutine(enemyToSpawn, spawnPoint, enemiesMaxCount));
     }
 
-    //ЗАЛИШИТИ
+    private void SpawnBuffOrDebuffLogic(GameObject prefab, Transform spawnPoint)
+    {
+        if (prefab.CompareTag("Arch"))
+        {
+            if (chunkSpawner.CanSpawnArch())
+            {
+                Instantiate(prefab, spawnPoint.position, spawnPoint.rotation, transform);
+                chunkSpawner.IncrementArchCount();
+            }
+        }
+
+        else if (prefab.CompareTag("BlueBox") || prefab.CompareTag("GreenBox"))
+        {
+            if (chunkSpawner.CanSpawnBox())
+            {
+                Instantiate(prefab, spawnPoint.position + Vector3.up * 0.5f, spawnPoint.rotation, transform);
+                chunkSpawner.IncrementBoxCount();
+            }
+        }
+    }
+
+    private void SpawnCoins()
+    {
+        if (_coinSpawnPoints == null || _coinSpawnPoints.Length == 0 || _coinPrefab == null)
+            return;
+
+        foreach (Transform point in _coinSpawnPoints)
+        {
+            Vector3 randomOffset = new Vector3(Random.Range(-1.8f, 1.8f), 0.5f, Random.Range(-1.8f, 1.8f));
+            Instantiate(_coinPrefab, point.position + randomOffset, point.rotation);
+        }
+    }
+
+    IEnumerator EnemySpawnCoroutine(GameObject randomPrefab, Transform randomPoint, int enemiesMaxCount)
+    {
+        int randomCount = Random.Range(_enemiesBaseMinCount, enemiesMaxCount);
+        if (chunkSpawner.totalChunksSpawned > 0 && chunkSpawner.totalChunksSpawned % chunkSpawner._chunksPerWave < 5)
+        {
+            randomCount = 0; 
+        }
+
+        for (int i = 0; i < randomCount; i++)
+        {
+            Instantiate(randomPrefab, randomPoint.position, randomPoint.rotation, transform);
+            yield return new WaitForSeconds(_enemySpawnTimer);
+        }
+    }
+
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -81,6 +215,5 @@ public class Chunk : MonoBehaviour
         }
     }
 }
-
 
 
--- 3D HyperCasual (learning proj)/Assets/Scripts/WorldScripts/LVLDesign/ChunkSpawner.cs
+++ 3D HyperCasual (learning proj)/Assets/Scripts/WorldScripts/LVLDesign/ChunkSpawner.cs
@@ -1,5 +1,4 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ChunkSpawner : MonoBehaviour
 {
@@ -7,7 +6,36 @@ public class ChunkSpawner : MonoBehaviour
     [Header("Chunk Settings")]
     [SerializeField] private GameObject _chunkPrefab; 
     public int chunksCountOnStart; 
-    public float chunkLenght; 
+    public float chunkLenght;
+
+    [Header("Wave Control")]
+    [Tooltip("Загальна кількість чанків, що з'явилися")]
+    public int totalChunksSpawned = 0; 
+    [Tooltip("Поточна хвиля ворогів")]
+    public int currentWave = 1;
+    [Tooltip("Чанків між хвилями. Після досягнення, хвиля збільшується.")]
+    public int _chunksPerWave = 10;
+
+
+    [Header("Wave Item Limits")]
+    [Tooltip("Максимальна кількість арок, які можуть з'явитися в межах однієї хвилі.")]
+    [SerializeField] private int _waveMaxArches = 5; 
+    [Tooltip("Максимальна кількість ящиків, які можуть з'явитися в межах однієї хвилі.")]
+    [SerializeField] private int _waveMaxBoxes = 3;
+
+    private int _currentWaveArchesCount = 0;
+    private int _currentWaveBoxesCount = 0;
+
+    [Header("Enemy Prefabs")]
+    [Tooltip("Префаби ЗВИЧАЙНИХ (ближній бій) ворогів.")]
+    [SerializeField] private GameObject[] _meleeEnemyPrefabs;
+    [Tooltip("Префаби СТРІЛЯЮЧИХ (дальній бій) ворогів.")]
+    [SerializeField] private GameObject[] _shootingEnemyPrefabs; 
+    [Tooltip("Хвиля, з якої починають спавнитися стріляючі вороги.")]
+    [SerializeField] private int _shootingEnemyStartWave = 10; 
+    public GameObject[] GetMeleePrefabs() => _meleeEnemyPrefabs;
+    public GameObject[] GetShootingPrefabs() => _shootingEnemyPrefabs;
+    public int GetShootingStartWave() => _shootingEnemyStartWave;
 
 
 
@@ -23,12 +51,37 @@ public class ChunkSpawner : MonoBehaviour
     public void StepOnChunk(Chunk currentChunk) 
     {
         SpawnChunk(currentChunk.transform.position + (Vector3.forward * chunksCountOnStart * chunkLenght));
+        totalChunksSpawned++;
+        if (totalChunksSpawned > 0 && (totalChunksSpawned / _chunksPerWave) >= currentWave)
+        {
+            currentWave++;
+            _currentWaveArchesCount = 0;
+            _currentWaveBoxesCount = 0;
+            Debug.Log($"Wave {currentWave - 1} completed! Starting Wave {currentWave}!");
+        }
+    }
+    public bool CanSpawnArch()
+    {
+        return _currentWaveArchesCount < _waveMaxArches;
     }
 
+    public void IncrementArchCount()
+    {
+        _currentWaveArchesCount++;
+    }
+
+    public bool CanSpawnBox()
+    {
+        return _currentWaveBoxesCount < _waveMaxBoxes;
+    }
+
+    public void IncrementBoxCount()
+    {
+        _currentWaveBoxesCount++;
+    }
     public void SpawnChunk(Vector3 spawnPosition) 
     {
         Chunk newChunk = Instantiate(_chunkPrefab, spawnPosition, Quaternion.identity, transform).GetComponent<Chunk>();
         newChunk.chunkSpawner = this;
     }
 }
-
